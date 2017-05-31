284786c9954e31f12ee5edbd519b3708c8732135
Filip Rogalski <f.rogalski@gmail.com>
2017-05-31 22:24:52 +0200

    Dodaję nowy formularz. Początek pracy nad skryptem js.

---
+++ workshop1-ver-rwd/js/summary.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('summary');
+    
+    var chairType = document.getElementById('chair-type'),
+        chairColor = document.getElementById('chair-color'),
+        chairFabric = document.getElementById('chair-fabric'),
+        transport = document.querySelector('[name=transport]');
+    
+    var updateSummary = function(){
+        
+    }
+    
+    
+});
