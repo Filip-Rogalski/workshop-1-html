f4766361cd54cd05b590a537637dc8f9e8e01959
Filip Rogalski <f.rogalski@gmail.com>
2017-06-01 21:48:49 +0200

    Dalsze prace nad order-form: podsumowanie: elementy, ceny, cena całkowita (błędy gdy user nie rozpocznie od wybrania typu fotela

--- workshop1-ver-rwd/js/summary.js
+++ workshop1-ver-rwd/js/summary.js
@@ -4,9 +4,9 @@ document.addEventListener('DOMContentLoaded', function () {
         , chairColor = document.getElementById('chair-color')
         , chairFabric = document.getElementById('chair-fabric')
         , transport = document.querySelector('[name=transport]')
-        , summaryContent = document.getElementById('summary-content');
-    
-    
+        , summaryContent = document.getElementById('summary-content')
+        , summaryPricelist = document.getElementById('summary-pricelist')
+        , summaryTotalSum = document.getElementById('summary-total-sum');
     var updateSummary = function () {
         summaryContent.innerHTML = '';
         var itemsChosen = document.createElement('p');
@@ -16,9 +16,42 @@ document.addEventListener('DOMContentLoaded', function () {
         };
         summaryContent.appendChild(itemsChosen);
     }
+    
+    var updatePricelist = function(){
+        summaryPricelist.innerHTML = '';
+        var typePrice = getOptionPrice(chairType);
+        var priceList = document.createElement('p');
+        priceList.innerHTML = typePrice + ' PLN<br /><br /><br />';
+        if (transport.checked) {
+            priceList.innerHTML += 90 + ' PLN';
+        }
+        summaryPricelist.appendChild(priceList);
+    }
+    
+    var getTotal = function(){
+        return parseInt(getOptionPrice(chairType)) + parseInt((transport.checked) * 90);
+    }
+    
+    var updateTotal = function(){
+        summaryTotalSum.innerHTML = '';
+        var totalSum = document.createElement('p');
+        var total = getTotal();
+        totalSum.innerHTML = total + ' PLN';
+        summaryTotalSum.appendChild(totalSum);
+    }
+    
     orderForm.addEventListener('change', function () {
         updateSummary();
+        updatePricelist();
+        updateTotal();
     });
-    
-    
+    var getOptionPrice = function (element) {
+        var elementValue = element.value;
+        var elementOptions = element.list.options;
+        for (var i = 0; i < elementOptions.length; i++) {
+            if (elementOptions[i].value == elementValue) {
+                return elementOptions[i].dataset.price;
+            }
+        }
+    }
 });
