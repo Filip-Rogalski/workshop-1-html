47cece966644ed88b838846c88c806351a5f78dc
Filip Rogalski <f.rogalski@gmail.com>
2017-06-01 21:12:10 +0200

    Dalsze prace nad order-form: podsumowanie cz. 1

--- workshop1-ver-rwd/js/summary.js
+++ workshop1-ver-rwd/js/summary.js
@@ -1,14 +1,24 @@
-document.addEventListener('DOMContentLoaded', function(){
-    console.log('summary');
+document.addEventListener('DOMContentLoaded', function () {
+    var orderForm = document.getElementById('order-form')
+        , chairType = document.getElementById('chair-type')
+        , chairColor = document.getElementById('chair-color')
+        , chairFabric = document.getElementById('chair-fabric')
+        , transport = document.querySelector('[name=transport]')
+        , summaryContent = document.getElementById('summary-content');
     
-    var chairType = document.getElementById('chair-type'),
-        chairColor = document.getElementById('chair-color'),
-        chairFabric = document.getElementById('chair-fabric'),
-        transport = document.querySelector('[name=transport]');
     
-    var updateSummary = function(){
-        
+    var updateSummary = function () {
+        summaryContent.innerHTML = '';
+        var itemsChosen = document.createElement('p');
+        itemsChosen.innerHTML = chairType.value + '<br /><span>' + chairColor.value + '</span><br /><span>' + chairFabric.value + '</span><br />';
+        if (transport.checked) {
+            itemsChosen.innerHTML += '<span>Transport</span>';
+        };
+        summaryContent.appendChild(itemsChosen);
     }
+    orderForm.addEventListener('change', function () {
+        updateSummary();
+    });
     
     
 });
